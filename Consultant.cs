f19f73c0b26b7e0a9c8c2766bded7e996ca0c96b
Tahir <Tahir@TAHIR->
2022-12-18 15:09:01 +0300

    Scattered classes in folders.

---
+++ Consultant.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+
+namespace BankSystem
+{
+    public class Consultant<IDType> : BankEmployee<IDType>, IClientDataChanging<IDType>, IClientDataReading<IDType>
+    {
+        public void ChangeDataOf(Client<IDType> client, Dictionary<string, string> data)
+        {
+            if (data.ContainsKey("Phone"))
+            {
+                client.Phone = data["Phone"];
+            }
+        }
+
+        public string Read(Client<IDType> client, string field)
+        {
+            PropertyInfo prop = client.GetType().GetProperty(field);
+
+            return prop.GetValue(client).ToString();
+        }
+    }
+}
---
+++ Manager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace BankSystem
+{
+    public class Manager<IDType> : BankEmployee<IDType>, IClientDataChanging<IDType>, IClientDataReading<IDType>
+    {
+        public void ChangeDataOf(Client<IDType> client, Dictionary<string, string> data)
+        {
+            Type type = client.GetType();
+            PropertyInfo prop;
+
+            foreach (KeyValuePair<string, string> pair in data)
+            {
+                prop = type.GetProperty(pair.Key);
+                prop.SetValue(client, pair.Value);
+            }
+        }
+
+        public string Read(Client<IDType> client, string field)
+        {
+            if (field == "Passport")
+            {
+                return client.GetPassport();
+            }
+
+            PropertyInfo prop = client.GetType().GetProperty(field);
+
+            return prop.GetValue(client).ToString();
+        }
+    }
+}
