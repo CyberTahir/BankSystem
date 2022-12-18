1175b744dd7fcc92b49e955da1b1da36688a7d2a
Tahir <Tahir@TAHIR->
2022-12-18 14:46:09 +0300

    Init.

---
+++ Account.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankSystem
+{
+    public class Account<IDType>
+    {
+        public IDType ID;
+        public int Money;
+
+        public Account(IDType id, int money = 0)
+        {
+            ID = id;
+            Money = money;
+        }
+    }
+}
---
+++ Client.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankSystem
+{
+    public class Client<IDType>
+    {
+        private string passport;
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FatherName { get; set; }
+        public string Name { get => LastName + " " + FirstName + " " + FatherName; }
+        public string Phone { get; set; }
+        public string Passport { get => "**** ******"; set => passport = value; }
+        public Account<IDType> Account { get; set; }
+
+        public Client(string firstN, string lastN, string fatherN, string _phone, string passportID, Account<IDType> account)
+        {
+            FirstName = firstN;
+            LastName = lastN;
+            FatherName = fatherN;
+            Phone = _phone;
+            Passport = passportID;
+            Account = account;
+        }
+
+        public string GetPassport()
+        {
+            return passport;
+        }
+    }
+}
